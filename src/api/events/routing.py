12a10738a179cdadcb060d045ec871ddac1cfe90
m-zeeshan-saeed <zeeshansheikh0313@gmail.com>
2025-10-17 17:15:11 +0500

    create fastapi

--- src/api/events/routing.py
+++ src/api/events/routing.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+from .schemas import EventSchema, EventListSchema
 
 router = APIRouter()
 
@@ -7,4 +8,18 @@ router = APIRouter()
 def read_events():
     return{
         "items":[1,2,3,4,5]
+    }
+
+
+@router.get("/{event_id}")
+def get_events(event_id:int) -> EventSchema :
+    return{
+        "id": event_id
+    }
+
+
+@router.get("/")
+def read_events()-> EventListSchema:
+    return{
+        "results":[{"id":1},{"id":2},{"id":3},{"id":4}]
     }
